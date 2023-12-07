0ea7ac97ce787413999d1604fef56959e3683fe2
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-07 15:51:48 +0800

    Update challenge_1_05.py

--- challenge_1_05.py
+++ challenge_1_05.py
@@ -2,10 +2,10 @@ from codrone_edu.drone import *
 drone = Drone()
 drone.pair()
 x = 20
-y = 15
+y = 30
 drone.takeoff()
 
-drone.set_pitch(10)
+drone.set_pitch(y)
 drone.move(1)
 drone.set_pitch(0)
 drone.set_roll(x)
@@ -23,7 +23,7 @@ drone.set_pitch(0)
 drone.set_roll(x)
 drone.move(1)
 drone.set_roll(0)
-drone.set_pitch(10)
+drone.set_pitch(y)
 drone.move(1)
 
 drone.land()
