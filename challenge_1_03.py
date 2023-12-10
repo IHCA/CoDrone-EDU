bb4ac3a16d5d528789f7ad14a18efb6db3f568ce
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 21:50:19 +0800

    Update challenge_1_03.py

--- challenge_1_03.py
+++ challenge_1_03.py
@@ -1,45 +1,29 @@
-'''from codrone_edu.drone import *
+#first challenge
+from codrone_edu.drone import * #引入CoDrone EDU的函式庫
+drone = Drone() #建立名為drone的CoDrone EDU物件
+drone.pair() #把遙控器跟無人機配對
+drone.takeoff() #讓無人機起飛並停在空中
+drone.set_pitch(40) #無人機向前方移動的功率設為40
+drone.move(3) #無人機移動3秒
+drone.land() #讓無人機輕輕降落到地面
+drone.close() #關閉無人機和遙控器的連線
 
-drone = Drone()
-drone.pair()
-print("Paired!")
-drone.takeoff()
-print("In the air!")
-drone.set_pitch(40) # set pitch value to +30
-drone.move(3)       # move with the set parameters for 1 second
-print("Landing")
-drone.land()
-drone.close()
-print("Program complete")
-'''
-from codrone_edu.drone import *
-drone = Drone()
-drone.pair()
+#second challenge
+from codrone_edu.drone import * #引入CoDrone EDU的函式庫
+drone = Drone() #建立名為drone的CoDrone EDU物件
+drone.pair() #把遙控器跟無人機配對
+drone.takeoff() #讓無人機起飛並停在空中
+drone.set_pitch(40) #無人機向前方移動的功率設為40
+drone.move(3) #無人機移動3秒
+drone.land() #讓無人機輕輕降落到地面
+drone.close() #關閉無人機和遙控器的連線
 
-drone.takeoff()
-
-drone.set_pitch(20)  # setting forward pitch
-
-drone.move(2)  # moving forward!
-
-drone.set_pitch(0)  # resetting pitch to 0
-
-drone.set_roll(20)  # setting roll to the right
-
-drone.move(2)  # moving right!
-
-drone.set_roll(0)  # resetting roll to 0
-
-drone.set_pitch(-20)  # setting backwards pitch
-
-drone.move(2)  # moving backwards!
-
-drone.set_pitch(0)  # resetting pitch to 0
-
-drone.set_roll(-20)  # setting roll to the left
-
-drone.move(2)  # moving left!
-
-drone.land()
-
-drone.close()
+#third challenge
+from codrone_edu.drone import * #引入CoDrone EDU的函式庫
+drone = Drone() #建立名為drone的CoDrone EDU物件
+drone.pair() #把遙控器跟無人機配對
+drone.takeoff() #讓無人機起飛並停在空中
+drone.set_pitch(40) #無人機向前方移動的功率設為40
+drone.move(3) #無人機移動3秒
+drone.land() #讓無人機輕輕降落到地面
+drone.close() #關閉無人機和遙控器的連線
