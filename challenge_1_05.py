18a0737219ce3f4e81899678d912b4f2326978cd
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 23:49:24 +0800

    Update challenge_1_05.py

--- challenge_1_05.py
+++ challenge_1_05.py
@@ -5,25 +5,25 @@ x = 20 #變數x的值設為20
 y = 30 #變數y的值設為30
 drone.takeoff() #讓無人機起飛並停在空中
 
-drone.set_pitch(y) #往前移動的值設為y，也就是30
+drone.set_pitch(y) #往前移動的功率設為y，也就是30
 drone.move(1) #無人機移動1秒
-drone.set_pitch(0) #往前移動的值設為0
-drone.set_roll(x) #往右移動的值設為x，也就是20
+drone.set_pitch(0) #往前移動的功率設為0
+drone.set_roll(x) #往右移動的功率設為x，也就是20
 drone.move(1) #無人機移動1秒
-drone.set_roll(0) #往右移動的值設為0
-drone.set_pitch(y) #往前移動的值設為y，也就是30
+drone.set_roll(0) #往右移動的功率設為0
+drone.set_pitch(y) #往前移動的功率設為y，也就是30
 drone.move(1) #無人機移動1秒
-drone.set_pitch(0) #往前移動的值設為0
-drone.set_roll(-x) #往左移動的值設為x，也就是20
+drone.set_pitch(0) #往前移動的功率設為0
+drone.set_roll(-x) #往左移動的功率設為x，也就是20
 drone.move(1) #無人機移動1秒
-drone.set_roll(0) #往左移動的值設為0
-drone.set_pitch(y) #往前移動的值設為y，也就是30
+drone.set_roll(0) #往左移動的功率設為0
+drone.set_pitch(y) #往前移動的功率設為y，也就是30
 drone.move(1) #無人機移動1秒
-drone.set_pitch(0) #往前移動的值設為0
-drone.set_roll(x) #往右移動的值設為x，也就是20
+drone.set_pitch(0) #往前移動的功率設為0
+drone.set_roll(x) #往右移動的功率設為x，也就是20
 drone.move(1) #無人機移動1秒
-drone.set_roll(0) #往右移動的值設為0
-drone.set_pitch(y) #往前移動的值設為y，也就是30
+drone.set_roll(0) #往右移動的功率設為0
+drone.set_pitch(y) #往前移動的功率設為y，也就是30
 drone.move(1) #無人機移動1秒
 
 drone.land() #讓無人機輕輕降落到地面
