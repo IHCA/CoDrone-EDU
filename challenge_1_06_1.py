4576054aaa0020d5e7445a40472de3d0c6d71249
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 22:33:50 +0800

    Update challenge_1_06_1.py

--- challenge_1_06_1.py
+++ challenge_1_06_1.py
@@ -1,24 +1,25 @@
-from codrone_edu.drone import *
-drone = Drone()
-drone.pair()
-power = 20
-duration = 1
-drone.takeoff()
+from codrone_edu.drone import * #引入CoDrone EDU的函式庫
+drone = Drone() #建立名為drone的CoDrone EDU物件
+drone.pair() #把遙控器跟無人機配對
+power = 20 #變數power的值設為20
+duration = 1 #變數duration的值設為1
+side = 0 #變數side的值設為0
+drone.takeoff() #讓無人機起飛並停在空中
 
-drone.set_pitch(power)
-drone.move(duration)
-drone.set_pitch(0)
-power = power / 2
-drone.set_roll(power)
-drone.move(duration)
-drone.set_roll(0)
-power = power + 10
-drone.set_pitch(-power)
-drone.move(duration)
-drone.set_pitch(0)
-power = 30 - power
-drone.set_roll(-power)
-drone.move(duration)
+drone.set_pitch(power) #往前移動的值設為power，也就是20
+drone.move(duration) #往前移動duration秒，也就是1秒
+drone.set_pitch(side) #往前移動的值設為side，也就是0
+power = power / 2 #power變成power/2，也就是10
+drone.set_roll(power) #往右移動的值設為power，也就是10
+drone.move(duration) #往前移動duration秒，也就是1秒
+drone.set_roll(side) #往右移動的值設為side，也就是0
+power = power + 10 #power變成power+10，也就是20
+drone.set_pitch(-power) #往後移動的值設為power，也就是20
+drone.move(duration) #往後移動duration秒，也就是1秒
+drone.set_pitch(side) #往後移動的值設為side，也就是0
+power = 30 - power #power變成30-power，也就是10
+drone.set_roll(-power) #往左移動的值設為power，也就是10
+drone.move(duration) #往左移動duration秒，也就是1秒
 
-drone.land()
-drone.close()
+drone.land() #讓無人機輕輕降落到地面
+drone.close() #關閉無人機和遙控器的連線
