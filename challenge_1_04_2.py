989c7bf33ddd5e1ed3354a573f5dffcfea98469a
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 22:08:18 +0800

    Update challenge_1_04_2.py

--- challenge_1_04_2.py
+++ challenge_1_04_2.py
@@ -1,21 +1,21 @@
-from codrone_edu.drone import *
-drone = Drone()
-drone.pair()
-drone.takeoff()
+from codrone_edu.drone import * #引入CoDrone EDU的函式庫
+drone = Drone() #建立名為drone的CoDrone EDU物件
+drone.pair() #把遙控器跟無人機配對
+drone.takeoff() #讓無人機起飛並停在空中
 
-drone.set_pitch(20)
-drone.set_throttle(30)
-drone.move(1)
-drone.set_throttle(-40)
-drone.move(1)
-drone.set_throttle(30)
-drone.move(1)
-drone.set_throttle(-40)
-drone.move(1)
-drone.set_throttle(30)
-drone.move(1)
-drone.set_throttle(-40)
-drone.move(1)
+drone.set_pitch(20) #無人機向前方移動的功率設為20
+drone.set_throttle(30) #無人機向上移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
+drone.set_throttle(-40) #無人機向下移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
+drone.set_throttle(30) #無人機向上移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
+drone.set_throttle(-40) #無人機向下移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
+drone.set_throttle(30) #無人機向上移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
+drone.set_throttle(-40) #無人機向下移動的功率設為30
+drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
 
-drone.land()
-drone.close()
+drone.land() #讓無人機輕輕降落到地面
+drone.close() #關閉無人機和遙控器的連線
