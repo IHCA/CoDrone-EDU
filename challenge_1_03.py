5e1ad1601cd2ad7084a825fe0b748aa7fff2c231
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 22:22:14 +0800

    Update challenge_1_03.py

--- challenge_1_03.py
+++ challenge_1_03.py
@@ -13,7 +13,7 @@ from codrone_edu.drone import * #引入CoDrone EDU的函式庫
 drone = Drone() #建立名為drone的CoDrone EDU物件
 drone.pair() #把遙控器跟無人機配對
 drone.takeoff() #讓無人機起飛並停在空中
-drone.set_pitch(40) #無人機向前方移動的功率設為40
+drone.set_roll(40) #無人機向右方移動的功率設為40
 drone.move(3) #無人機移動3秒
 drone.land() #讓無人機輕輕降落到地面
 drone.close() #關閉無人機和遙控器的連線
@@ -24,6 +24,12 @@ drone = Drone() #建立名為drone的CoDrone EDU物件
 drone.pair() #把遙控器跟無人機配對
 drone.takeoff() #讓無人機起飛並停在空中
 drone.set_pitch(40) #無人機向前方移動的功率設為40
-drone.move(3) #無人機移動3秒
+drone.move(2) #無人機移動2秒
+drone.set_pitch(0) #無人機向前方移動的功率設為0
+drone.set_roll(-40) #無人機向左方移動的功率設為40
+drone.move(2) #無人機移動2秒
+drone.set_roll(0) #無人機向左方移動的功率設為0
+drone.set_pitch(40) #無人機向前方移動的功率設為40
+drone.move(2) #無人機移動2秒
 drone.land() #讓無人機輕輕降落到地面
 drone.close() #關閉無人機和遙控器的連線
