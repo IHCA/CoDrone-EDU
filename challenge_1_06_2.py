dbe39c5ceee8f9c4cfac101f47a4b296cc5d4962
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-10 23:50:41 +0800

    Update challenge_1_06_2.py

--- challenge_1_06_2.py
+++ challenge_1_06_2.py
@@ -9,17 +9,17 @@ turn = 50 #變數turn的值設為50
 duration = 1 #變數duration的值設為1
 drone.takeoff() #讓無人機起飛並停在空中
 
-drone.set_pitch(power) #往前移動的值設為power，也就是25
+drone.set_pitch(power) #往前移動的功率設為power，也就是25
 drone.move(duration) #往前移動duration秒，也就是1秒
-drone.set_pitch(0) #往前移動的值設為0
-drone.set_yaw(turn) #逆時針用turn的值旋轉，也就是50的值旋轉
+drone.set_pitch(0) #往前移動的功率設為0
+drone.set_yaw(turn) #逆時針用turn的功率旋轉，也就是50的功率旋轉
 drone.move(duration) #往前移動duration秒，也就是1秒
-drone.set_yaw(0) #旋轉的值設為0
+drone.set_yaw(0) #旋轉的功率設為0
 power = power + 5 #power從25變成30
-drone.set_pitch(power) #往前移動的值設為power，也就是30
+drone.set_pitch(power) #往前移動的功率設為power，也就是30
 drone.move(duration) #往前移動duration秒，也就是1秒
-drone.set_pitch(0) #往前移動的值設為0
-drone.set_yaw(turn) #逆時針用turn的值旋轉，也就是50的值旋轉
+drone.set_pitch(0) #往前移動的功率設為0
+drone.set_yaw(turn) #逆時針用turn的功率旋轉，也就是50的功率旋轉
 drone.move(duration) #往前移動duration秒，也就是1秒
 '''
 drone.set_yaw(0)
