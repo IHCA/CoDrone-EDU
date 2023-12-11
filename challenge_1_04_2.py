517059309aadf02957214010f2a2f946a5405978
IHCA <83703795+IHCA@users.noreply.github.com>
2023-12-11 21:24:32 +0800

    Update challenge_1_04_2.py

--- challenge_1_04_2.py
+++ challenge_1_04_2.py
@@ -6,15 +6,15 @@ drone.takeoff() #讓無人機起飛並停在空中
 drone.set_pitch(20) #無人機向前方移動的功率設為20
 drone.set_throttle(30) #無人機向上移動的功率設為30
 drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
-drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來不明顯，不像sin波，因此將數值稍微調大
+drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來比較小，不像sin波，因此將數值稍微調大
 drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
 drone.set_throttle(30) #無人機向上移動的功率設為30
 drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
-drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來不明顯，不像sin波，因此將數值稍微調大
+drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來比較小，不像sin波，因此將數值稍微調大
 drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
 drone.set_throttle(30) #無人機向上移動的功率設為30
 drone.move(1) #無人機移動1秒，因為pitch跟throttle都是正數所以會同時往前跟往上移動
-drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來不明顯，不像sin波，因此將數值稍微調大
+drone.set_throttle(-40) #無人機向下移動的功率設為40，本來應該要是30才會顯示sin波，但因為飛行中的偏差讓往下飛的幅度看起來比較小，不像sin波，因此將數值稍微調大
 drone.move(1) #無人機移動1秒，因為pitch為正，throttle為負，所以會同時往前跟往下移動
 
 drone.land() #讓無人機輕輕降落到地面
